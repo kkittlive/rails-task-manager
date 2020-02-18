544000b7e400dba8bdbcf31fb0f7da84b76fe125
Janette Kwan <kkitt@live.com>
2020-02-18 16:19:10 -0300

    finished everything

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tasks
+
+  # # Route for viewing all tasks
+  # get 'tasks', to: 'tasks#index', as: :tasks
+
+  # # Routes for creating a new task
+  # get 'tasks/new', to: 'tasks#new', as: :new
+  # post 'tasks', to: 'tasks#create'
+
+  # # Route for viewing one task's details
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # # Route for editing a task
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  # patch 'tasks/:id', to: 'tasks#update'
+
+  # # Route for deleting a task
+  # delete 'tasks/:id', to: 'tasks#destroy'
 end
